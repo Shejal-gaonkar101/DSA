0820b7232a78ca22033f3cc4ee1f4d84323f9a5d
Shejal-gaonkar101 <shejalgaonkar2001@gmail.com>
2025-08-03 19:02:36 +0530

    New File Added

---
+++ reverseArray.js
@@ -0,0 +1,29 @@
+
+class reverseArray {
+  constructor() {
+    this.#reverse([45, 85, 14, 20, 13, 8])
+  }
+
+
+  #reverse(arr) {
+    for (var i = 0; i < arr.length - 1; i++) {
+      var minimum = i
+      for (var j = i + 1; j < arr.length; j++) {
+
+        if (arr[j] < arr[minimum]) {
+          var minimum = j
+        }
+      }
+      if (minimum !== i) {
+        let temp = arr[i];
+        arr[i] = arr[minimum];
+        arr[minimum] = temp;
+      }
+      console.log(arr); 
+    }
+
+
+
+
+  }
+} 
