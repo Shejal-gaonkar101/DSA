c5a7d9d83f23a34d22c35af8979bf711ee9cafcc
Shejal-gaonkar101 <shejalgaonkar2001@gmail.com>
2025-08-11 23:38:11 +0530

    promise notes

---
+++ JavaScript/Promise.js
@@ -0,0 +1,45 @@
+// console.log("hello");
+// setTimeout(function(){
+// console.log("hiii");
+// },5000)
+
+// console.log("time Out");
+
+// //example of promise 
+//  const cart = ["shoes","pants","kurtas"]
+//  createOrder(cart,function(orderId){
+//     proceedtopay(orderId)
+//  })
+
+// const promise =  createorder(cart)
+// promise.then(function(orderId){
+//     proceedtoPaymnet(orderId)
+// })  
+
+promise.catch(function(err){
+    console.log("error",err);
+    
+})
+
+//fetch()
+
+const api = "https://github.com/Shejal-gaonkar101";
+let data = fetch(api)
+
+console.log(data );
+
+//Creating an pormise 
+function createOrder    (cart){
+const pr = new Promise(function(resolve ,reject){
+    if(!data){
+        const err = new console.error("data not foung");
+        
+        reject(err)
+    }
+    else{
+        resolve(orderID =12454)
+    }
+
+})
+}
+
